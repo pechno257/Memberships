2db1db7aa709e8fec4e1e2fef5d8d9297a81a19f
Jean Petit <altoch@hotmail.com>
2018-10-12 22:18:13 -0400

    Added Part class

---
+++ Memberships/Entities/Part.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Memberships.Entities
+{
+    [Table("Part")]
+    public class Part
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [MaxLength(255)]
+        public string Title { get; set; }
+    }
+}
