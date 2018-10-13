e6ac8ef770acaf92f10dbfe30ea7c76deb070d94
Jean Petit <altoch@hotmail.com>
2018-10-12 21:44:45 -0400

    Added a DBSet for the Section class

--- Memberships/Models/IdentityModels.cs
+++ Memberships/Models/IdentityModels.cs
@@ -20,6 +20,8 @@ namespace Memberships.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Section> Sections { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
