ad62ef9f196e6564d3d9c05e16bbe5b14d83f434
Jean Petit <altoch@hotmail.com>
2018-10-16 22:59:58 -0400

    Updated Database

---
+++ Memberships/Entities/ProductLinkText.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Memberships.Entities
+{
+    [Table("ProductLinkText")]
+    public class ProductLinkText
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [MaxLength(255)]
+        [Required]
+        public string Title { get; set; }
+    }
+}
---
+++ Memberships/Entities/Subscription.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Memberships.Entities
+{
+    [Table("Subscription")]
+    public class Subscription
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [MaxLength(255)]
+        [Required]
+        public string Title { get; set; }
+
+        [MaxLength(2048)]
+        public string Description { get; set; }
+
+        [MaxLength(20)]
+        [DisplayName("Registration Code")]
+        public string RegistrationCode { get; set; }
+    }
+}
